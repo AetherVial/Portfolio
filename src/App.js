fd92e115ae8736a1c378b575a2eaf29dfe24a730
Stanton Huang <stantonhuang11@gmail.com>
2019-10-16 17:47:40 -0700

    initial

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
 import React from 'react';
-import logo from './logo.svg';
+import {BrowserRouter, Route} from 'react-router-dom';
 import './App.css';
+import Sidebar from './sidebar';
+import Links from './links';
+import Home from './home';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <Route path="/" component={Links}/>
+        <Route path="/" component={Sidebar}/>
+        <Route exact path="/" component={Home}/>
+      </div>
+    </BrowserRouter>
   );
 }
 
