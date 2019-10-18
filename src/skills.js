22dc68afc07115eb9642fa7ae4e083fe2be66740
Stanton Huang <stantonhuang11@gmail.com>
2019-10-18 11:12:33 -0700

    adds skills list on side

--- src/skills.js
+++ src/skills.js
@@ -1,9 +1,6 @@
 import React from 'react';
 import './styles/home.css';
 
-const today = new Date();
-const time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
-
 const Skills = () => (
     <div className="home">
         <nav>
@@ -104,6 +101,16 @@ const Skills = () => (
                 c0.444,0.444,1.159,0.444,1.587,0l9.952-9.952c0.444-0.429,0.444-1.143,0-1.571L21.205,5.007z"/>
             </svg>
                 Roses are red. Violets are blue. Express.js is in the channel with you. 
+            </div>
+
+            <div className='entered-channel'>
+                <svg version="1.1" id="Capa_1" xmlns="http://www.w3.org/2000/svg" x="0px" y="0px"
+                viewBox="0 0 31.49 31.49">
+                <path style={{fill:'#43b581'}} d="M21.205,5.007c-0.429-0.444-1.143-0.444-1.587,0c-0.429,0.429-0.429,1.143,0,1.571l8.047,8.047H1.111
+                C0.492,14.626,0,15.118,0,15.737c0,0.619,0.492,1.127,1.111,1.127h26.554l-8.047,8.032c-0.429,0.444-0.429,1.159,0,1.587
+                c0.444,0.444,1.159,0.444,1.587,0l9.952-9.952c0.444-0.429,0.444-1.143,0-1.571L21.205,5.007z"/>
+            </svg>
+                Git has commited... to this channel!
             </div>
         </div>
     </div>
