e587ffd0ccbf8d8246c2eb14a53ec4761ab2987e
stanton huang <stantonhuang@stantons-MacBook-Pro.local>
2024-03-07 15:40:49 -0800

    removes excess from beginning chat:

--- src/components/beginningChat.js
+++ src/components/beginningChat.js
@@ -1,9 +1,7 @@
 import '../styles/basicChat.css';
 import React from 'react';
 
-const today = new Date();
-const time = today.getHours() + ":" + today.getMinutes();
-const BeginningChat = ({children}) => {
+const BeginningChat = () => {
     return (
         <div className='beginning'>
             This is the beginning of your chat history with @Stanton.
